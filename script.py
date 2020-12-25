3dc03e0ce3174d43de06eb9e560d80e1dd51d1f8
2h311 <kcodepal@outlook.com>
2020-12-25 17:48:11 +0100

    Initial project version

---
+++ script.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+def create_driver_instance(driver_path = "./chromedriver/chromedriver.exe"):
+	chrome_options = Options()
+	chrome_options.add_argument("start-maximized")
+	chrome_options.add_argument("log-level=3")
+	chrome_options.add_experimental_option("useAutomationExtension", False)
+	chrome_options.add_experimental_option("excludeSwitches",["enable-automation"])
+	return webdriver.Chrome(executable_path=driver_path, options=chrome_options)
+
+driver = create_driver_instance()
+driver.get('https://google.com')
